d46c84f275b9bacd51370fb14f74e1d7aa822d96
unkochi <anais.picaut@gmail.com>
2026-02-10 21:52:47 +0100

    added a file

--- src/App.jsx
+++ src/App.jsx
@@ -157,7 +157,7 @@ function App() {
               path="/documents"
               element={
                 <ProtectedRoute>
-                  <Documents />
+                  <Docu />
                 </ProtectedRoute>
               }
             />
